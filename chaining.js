6bc8bd41068648e5a2f3aefb3acf7d6adb25aa05
RatonRoy <royraton18@gmail.com>
2022-02-23 00:05:38 +0600

    hello I want to clear array and objects

---
+++ chaining.js
@@ -0,0 +1,6 @@
+// declere variable based on the object of the property 
+const { fullName, age, phone } = { fullName: 'kabir sing', age: 23, phone: 'samsung' };
+console.log(fullName, age, phone);
+// declere variable based on the array 
+const [first, second, third, four, five] = [12, 23, 32, 45, 56];
+console.log(first, second, third, four, five); 
